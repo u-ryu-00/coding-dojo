5897169ed418e3aef58cf301a3f7982f57ecdd3c
류유정 <ryuyoojung@lyuyujeong-ui-MacBookAir.local>
2023-01-31 09:55:35 +0900

    BMI
    BMI 계산기를 만들었음.

---
+++ 20230131 - BMI/BMI/src/Bmi.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Bmi {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        Double height = scanner.nextDouble();
+        Double weight = scanner.nextDouble();
+        Double bmi = weight / (height*height);
+
+        System.out.println("BMI 측정기");
+        System.out.println("신장(m): " + height);
+        System.out.println("체중(kg): " + weight);
+
+        if(bmi <= 18.5){
+            System.out.println("저체중");
+        }
+
+        if(bmi > 18.5 && bmi <= 23){
+            System.out.println("정상");
+        }
+
+        if(bmi > 23 && bmi <= 25){
+           System.out.println("과체중");
+        }
+
+        if(bmi > 25){
+            System.out.println("비만");
+        }
+
+        System.out.println("BMI: " + bmi);
+
+    }
+}
