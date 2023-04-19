6383673e86d210bb5140550870ead4b5d0928b2d
u-ryu-00 <lwh76104@gmail.com>
2023-04-19 10:00:05 +0900

    정수 내림차순으로 배치하기
    정수 내림차순으로 배치하기 프로그램을 완성함.

---
+++ 20230419-정수내림차순으로배치하기/Integer-자바스크립트/solution.js
@@ -0,0 +1,17 @@
+export default function solution(n) {
+  const answer = `${n}`;
+  const answer2 = answer.split('');
+  return Number(answer2.sort((a, b) => b - a).join(''));
+}
+
+/*
+계획
+- 입력받은 정수 n을 문자열 형태로 변환한다.
+- 변환한 문자열을 split으로 한글자 한글자 자른다.
+
+sort((a, b) => b - a)
+- 숫자 배열을 내림차순으로 정렬하는 데 사용된다.
+- sort 메서드는 비교함수를 정렬 순서를 정의하는 인수로 사용한다.
+- 위의 코드에서 비교 함수는 (a, b) => b - a로,
+a와 b 두 요소를 비교하여 b에서 a를 뺀 결과를 반환한다.
+*/
