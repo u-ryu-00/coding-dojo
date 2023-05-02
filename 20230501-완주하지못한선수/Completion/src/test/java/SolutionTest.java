a78b79590de2b1e10835ae3e4bc230bb40cab0d6
u-ryu-00 <lwh76104@gmail.com>
2023-05-02 09:30:40 +0900

    테스트 코드 추가

--- 20230501-완주하지못한선수/Completion/src/test/java/SolutionTest.java
+++ 20230501-완주하지못한선수/Completion/src/test/java/SolutionTest.java
@@ -1,12 +1,13 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionTest {
     @Test
-    void Simple(){
+    void Simple() {
         Solution solution = new Solution();
-
-
+        assertEquals("leo", solution.solution(new String[]{"leo", "kiki", "eden"}, new String[]{"eden", "kiki"}));
+        assertEquals("vinko", solution.solution(new String[]{"marina", "josipa", "nikola", "vinko", "filipa"}, new String[]{"josipa", "filipa", "marina", "nikola"}));
+        assertEquals("mislav", solution.solution(new String[]{"mislav", "stanko", "mislav", "ana"}, new String[]{"stanko", "ana", "mislav"}));
     }
 }
