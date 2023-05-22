8f37bb75996c7b926a0efc137c2b06d93fbcda9d
u-ryu-00 <lwh76104@gmail.com>
2023-05-22 09:17:39 +0900

    20230522-모든레코드조회하기

---
+++ 20230522-모든레코드조회하기/Recode/Solution.sql
@@ -0,0 +1,12 @@
+SELECT * FROM ANIMAL_INS ORDER BY ANIMAL_ID
+
+/*
+ANIMAL_INS 테이블 : 동물 보호소에 들어온 동물의 정보를 담은 테이블
+아래와 같은 정보들이 담겨 있다. 
+ANIMAL_ID, -> 동물의 아이디
+ANIMAL_TYPE, -> 생물 종
+DATETIME, -> 보호 시작일
+INTAKE_CONDITION, -> 보호 시작 시 상태
+NAME, -> 이름
+SEX_UPON_INTAKE -> 성별 및 중성화 여부
+*/
