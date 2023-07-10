09991311cd6dd746bbdfb4fa6cdb2c5b63f117e4
u-ryu-00 <lwh76104@gmail.com>
2023-07-10 09:58:19 +0900

    영어 끝말잇기
    영어 끝말잇기 문제를 자바 언어로 풀이하였음.

---
+++ 20230710-영어끝말잇기/Chain/src/test/java/SolutionTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+    @Test
+    void Simple() {
+        Solution solution = new Solution();
+        assertArrayEquals(new int[]{3, 3}, solution.solution(3, new String[]{"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"}));
+        assertArrayEquals(new int[]{0, 0}, solution.solution(5, new String[]{"hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"}));
+        assertArrayEquals(new int[]{1, 3}, solution.solution(2, new String[]{"hello", "one", "even", "never", "now", "world", "draw"}));
+    }
+}
