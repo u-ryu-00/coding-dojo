34df60fa9b2a95a63de113a5a87e1b9e36fe1cb6
u-ryu-00 <lwh76104@gmail.com>
2023-07-11 22:09:51 +0900

    20230712-구명보트
    구명보트 문제를 자바언어로 풀이하였음.

---
+++ 20230712-구명보트/Boat/src/test/java/SolutionTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+    @Test
+    void Simple() {
+        Solution solution = new Solution();
+        assertEquals(3, solution.solution(new int[]{70, 50, 80, 50}, 100));
+        assertEquals(3, solution.solution(new int[]{70, 80, 50}, 100));
+    }
+}
